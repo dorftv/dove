8e41d129b5d5d3dc736b07d3ce5eadc76efc3da3
hatsch <hatsch@servus.at>
2023-12-11 15:02:56 +0100

    refactor websockets handling.

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -13,23 +13,52 @@
 </template>
 
 <script setup>
-
-import { watch,  ref, onMounted, onUnmounted } from 'vue';
+import { watch, ref, onMounted, onUnmounted } from 'vue';
 
 const webSocket = ref(null);
-const { data: inputs } = await useFetch('/api/inputs')
-const { data: mixers } = await useFetch('/api/mixers')
-const { data: outputs } = await useFetch('/api/inputs')
+const { data: inputs } = await useFetch('/api/inputs');
+const { data: mixers } = await useFetch('/api/mixers');
+const { data: outputs } = await useFetch('/api/outputs');
 
+const entityMap = {
+  input: inputs,
+  mixer: mixers,
+  output: outputs
+};
 
+const updateEntity = (type, updatedEntity) => {
+  const entities = entityMap[type].value;
+  const index = entities.findIndex(entity => entity.uid === updatedEntity.uid);
+  if (index !== -1) {
+    entities[index] = { ...entities[index], ...updatedEntity };
+  }
+};
+
+const addEntity = (type, newEntity) => {
+  const entities = entityMap[type].value;
+  if (!entities.some(entity => entity.uid === newEntity.uid)) {
+    entities.push(newEntity);
+  }
+};
+
+const deleteEntity = (type, entityToDelete) => {
+  entityMap[type].value = entityMap[type].value.filter(entity => entity.uid !== entityToDelete.uid);
+};
 
 onMounted(() => {
   webSocket.value = new WebSocket('ws://localhost:9999');
-
   webSocket.value.onmessage = (event) => {
     const message = JSON.parse(event.data);
-    if (message.type === 'input') {
-      handleInputMessage(message.type, message.channel, message.data);
+    const actionMap = {
+      CREATE: addEntity,
+      UPDATE: updateEntity,
+      DELETE: deleteEntity
+    };
+    const action = actionMap[message.channel];
+    if (action && entityMap[message.type]) {
+      action(message.type, message.data);
+    } else {
+      console.warn('Unknown Type or Channel:', message.type, message.channel);
     }
   };
 
@@ -44,103 +73,18 @@ onUnmounted(() => {
   }
 });
 
-const handleInputMessage = (type, channel, data) => {
-  switch (channel) {
-    case 'CREATE':
-      if      (type === 'input') addInput(data)
-      else if (type === 'mixer') addMixer(data)
-      else if (type === 'output') addOutput(data)
-      else console.warn('Unknown Type:',type);
-      break;
-    case 'UPDATE':
-      if      (type === 'input') updateInput(data)
-      else if (type === 'mixer') updateMixer(data)
-      else if (type === 'output') updateOutput(data)
-      else console.warn('Unknown Type:',type);
-      break;
-      break;
-    case 'DELETE':
-      if      (type === 'input') deleteInput(data)
-      else if (type === 'mixer') deleteMixer(data)
-      else if (type === 'output') deleteOutput(data)
-      else console.warn('Unknown Type:',type);
-      break;
-      break;
-    default:
-      console.warn('Unhandled channel:', channel);
-  }
+const watchEntities = (entities) => {
+  watch(entities, (newEntities, oldEntities) => {
+    // Handle entity changes
+  }, { deep: true });
 };
 
-// Handle Inputs Updates
-const updateInput = (updatedInput) => {
-  const index = inputs.value.findIndex(input => input.uid === updatedInput.uid);
-  if (index !== -1) {
-    inputs.value[index] = { ...inputs.value[index], ...updatedInput };
-  }
-};
-
-const addInput = (newInput) => {
-  const exists = inputs.value.some(input => input.uid === newInput.uid);
-  if (!exists) {
-    inputs.value.push(newInput);
-  }
-};
-
-const deleteInput = (inputToDelete) => {
-  inputs.value = inputs.value.filter(input => input.uid !== inputToDelete.uid);
-};
-
-watch(inputs, (newInput, oldInput) => {
-
-}, { deep: true });
-
-// Handle mixers Updates
-const updateMixer = (updatedMixer) => {
-  const index = mixers.value.findIndex(mixer => mixer.uid === updatedMixer.uid);
-  if (index !== -1) {
-    mixers.value[index] = { ...mixers.value[index], ...updatedMixer };
-  }
-};
-
-const addMixer = (newMixer) => {
-  const exists = mixers.value.some(mixer => mixer.uid === newMixer.uid);
-  if (!exists) {
-    mixers.value.push(newMixer);
-  }
-};
-
-const deleteMixer = (mixerToDelete) => {
-  mixers.value = mixers.value.filter(mixer => mixer.uid !== mixerToDelete.uid);
-};
-
-watch(mixers, (newMixer, oldmixer) => {
-
-}, { deep: true });
-
-// Handle outputs Updates
-const updateOutput = (updatedOutput) => {
-  const index = outputs.value.findIndex(output => output.uid === updatedOutput.uid);
-  if (index !== -1) {
-    outputs.value[index] = { ...outputs.value[index], ...updatedOutput };
-  }
-};
-
-const addOutput = (newOutput) => {
-  const exists = outputs.value.some(output => output.uid === newOutput.uid);
-  if (!exists) {
-    outputs.value.push(newOutput);
-  }
-};
-
-const deleteOutput = (outputToDelete) => {
-  outputs.value = outputs.value.filter(output => output.uid !== outputToDelete.uid);
-};
-
-watch(outputs, (newOutput, oldOutput) => {
-
-}, { deep: true });
+watchEntities(inputs);
+watchEntities(mixers);
+watchEntities(outputs);
 
 </script>
-<style scoped>  
+
+<style scoped>
 
 </style>

acf93029e3690a5373d1037a71b3f0d1eaa1f07a
hatsch <hatsch@servus.at>
2023-12-21 14:49:15 +0100

    add fixed.

--- api/pipelines/outputs/output.py
+++ api/pipelines/outputs/output.py
@@ -8,10 +8,10 @@ class Output(GSTBase, ABC):
     uid: UUID
     src: UUID
     def get_video_start(self) -> str:
-        return f" interpipesrc name=video_{uid} listen-to={src} "
+        return f" interpipesrc name=video_{self.uid} listen-to={self.src} "
 
     def get_audio_start(self):
-        return f" interpipesrc name=audio_{uid} listen-to={src} "
+        return f" interpipesrc name=audio_{self.uid} listen-to={self.src} "
 
     def describe(self):
 

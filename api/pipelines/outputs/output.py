e035e1ea874c86c86845d0c4972c34dd326f250f
hatsch <hatsch@servus.at>
2023-12-29 12:22:33 +0100

    add helper functions to base. WIP: add has_audio handling.

--- api/pipelines/outputs/output.py
+++ api/pipelines/outputs/output.py
@@ -8,10 +8,10 @@ class Output(GSTBase, ABC):
     uid: UUID
     src: UUID
     def get_video_start(self) -> str:
-        return f" interpipesrc name=video_{self.uid} listen-to=video_{self.src} is-live=true format=time allow-renegotiation=true ! "
+        return f" interpipesrc name=video_{self.uid} listen-to=video_{self.src} is-live=true format=time allow-renegotiation=false  stream-sync=restart-ts ! queue !  "
 
     def get_audio_start(self):
-        return f" interpipesrc name=audio_{self.uid} listen-to=audio_{self.src} is-live=true format=time allow-renegotiation=true ! "
+        return f" interpipesrc name=audio_{self.uid} listen-to=audio_{self.src} is-live=true format=time allow-renegotiation=false is-live=true stream-sync=restart-ts stream-sync=restart-ts ! queue ! "
 
     def describe(self):
         return self
--- api/pipelines/outputs/preview_hls_output.py
+++ api/pipelines/outputs/preview_hls_output.py
@@ -13,14 +13,23 @@ class previewHlsOutput(Output):
         preview_path = self.output_base.joinpath(str(self.data.src))
         if not preview_path.is_dir():
             preview_path.mkdir(parents=True, exist_ok=False)
-
+        
         audio_caps = "audio/x-raw, format=S16LE, layout=(string)interleaved, rate=(int)44100, channels=(int)2"
 
-        self.add_pipeline(self.get_video_start() + f" videoconvert ! videoscale ! videorate ! video/x-raw,format=I420 ! queue !  "
-        f" x264enc  speed-preset=ultrafast ! video/x-h264,profile=main ! queue ! mpegtsmux name=mux ! "
+        # @TODO get source element
+        pipeline_audio_str = ""
+        if self.has_audio_or_video("audio"):
+                pipeline_audio_str = f" {self.get_audio_start()}  audioconvert ! audioresample ! {audio_caps} ! voaacenc  ! aacparse !  queue ! mux."
+
+
+        self.add_pipeline(self.get_video_start() + f" videoconvert ! videoscale ! videorate !  video/x-raw,width=320,height=180 ! queue  !  "
+        f" x264enc  speed-preset=ultrafast ! video/x-h264,profile=baseline ! mpegtsmux name=mux ! "
         f" hlssink async-handling=true target-duration=1  max-files=5 "
         f" playlist-location={preview_path.joinpath('index.m3u8')} location={preview_path.joinpath('segment%05d.ts')} "
-        f" {self.get_audio_start()}  audioconvert ! audioresample ! {audio_caps} ! voaacenc  ! aacparse !  queue ! mux.")
+        f" { pipeline_audio_str }")
+
+        
+        
 
     def describe(self):
         return self.data

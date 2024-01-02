cf2204da177a793349589301ec7153ad5e2c5dd8
hatsch <hatsch@servus.at>
2024-01-02 12:41:29 +0100

    provide hls library.

--- frontend/components/VideoPlayer.vue
+++ frontend/components/VideoPlayer.vue
@@ -2,7 +2,7 @@
 <template>
   <div>     
 
-    <media-player ref="player" :muted="muted" viewType="video" autoplay stream-type="live" load="eager" title="Sprite Fight"  :src="`/preview/hls/${uid}/index.m3u8`" class="aspect-video">
+    <media-player ref="mediaPlayer" :muted="muted" viewType="video" autoplay stream-type="live" load="eager" title="Sprite Fight"  :src="`/preview/hls/${uid}/index.m3u8`" class="aspect-video">
       <media-provider></media-provider>
       <media-video-layout></media-video-layout>
 
@@ -17,23 +17,34 @@ import 'vidstack/player/layouts';
 import 'vidstack/player/ui';
 import 'vidstack/player/styles/default/theme.css';
 import 'vidstack/player/styles/default/layouts/video.css';
-
+import HLS from 'hls.js';
 
 import { MediaPlayerElement } from 'vidstack/elements';
 
-/*
-const player = ref(MediaPlayerElement);
-console.log(player) 
-player.addEventListener('hls-error', (event) => {
-  console.log;
+const mediaPlayer = ref(null);
+
+onMounted(() => {
+  const player = mediaPlayer.value;
+
+  player.addEventListener('provider-change', (event) => {
+  const provider = event.detail;
+  if (provider?.type === 'hls') {
+    provider.library = HLS;
+  }
 });
 
-*/
+player.addEventListener('can-play', () => {
+  player.play()
+  console.log(player.state)
+});
 
+  player.addEventListener('hls-error', (event) => {
+    const provider = event.detail;
+    const src = player.src
+    // TODO find a way to handle error
+ });
+});
 
-
-
-//console.log(player)
 const props = defineProps({
   uid: String,
   muted: String

7b05b60da8331f7e3a965dfb3ab4bd5c1a212823
hatsch <hatsch@servus.at>
2024-07-23 12:29:23 +0200

    update frontend. improve proxy handling. improve muting player.

---
+++ static/_nuxt/B5he5d6x.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./CMPmiJih.js";import"./Dd1UHcEZ.js";import"./D5ZdezSB.js";import"./BTpZe_-F.js";export{o as default};
---
+++ static/_nuxt/DDsfGVM1.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./Dd1UHcEZ.js";import"./D5ZdezSB.js";export{m as default};

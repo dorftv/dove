dc0e5838f5cf5b3cd92be7b12a6d07b8a7951a1a
hatsch <hatsch@servus.at>
2024-09-15 21:37:04 +0200

    add proxy for local mediamtx streams.

---
+++ proxy/mediamtx.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter
+import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import urllib.parse
+
+from config_handler import ConfigReader
+
+config = ConfigReader()
+router = APIRouter()
+
+@router.get("/proxy/mediamtx")
+def proxy_get():
+    relay_names = config.get_proxy("mediamtx")
+
+    def fetch_url(name):
+        details = config.get_proxy_details("mediamtx", name)
+        if not details:
+            return []
+        try:
+            if not isinstance(details, dict):
+                return []
+
+            response = requests.get(details['uri'], timeout=5)
+            response.raise_for_status()
+            data = response.json()
+
+            if isinstance(data, list):
+                for item in data:
+                    if 'url' in item:
+                        item['uri'] = f"{item['url']}/{urllib.parse.quote(details['auth'])}"
+                        del item['url']
+                    if 'name' in item:
+                        item['name'] = f"{name}/{item['name']}"
+            return data
+        except requests.RequestException as e:
+            return []
+
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(fetch_url, name) for name in relay_names]
+        merged_results = []
+        for future in as_completed(futures):
+            data = future.result()
+            if data:
+                merged_results.extend(data)
+
+    return merged_results

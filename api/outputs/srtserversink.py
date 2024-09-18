ff117ddba0b47e35936d96c172ec511f26552b9c
hatsch <hatsch@servus.at>
2024-09-18 16:23:02 +0200

    add new output: srtserversink.

---
+++ api/outputs/srtserversink.py
@@ -0,0 +1,88 @@
+from fastapi import APIRouter, Request
+from pydantic import Field
+from api.output_models import OutputDTO, SuccessDTO
+from typing import Optional
+
+from api.websockets import manager
+
+
+
+router = APIRouter()
+
+# @TODO improve codec handling
+class SrtserversinkOutputDTO(OutputDTO):
+    type: str = Field(
+        label="SRT Sink",
+        default="srtserversink",
+        description="stream output to SRT Server.",
+    )
+    uri: str = Field(
+        label="Uri",
+        description="Enter SRT Server URL and Port",
+        placeholder="srt://server:port"
+    )
+
+    streamid: Optional[str] = Field(
+        default=None,
+        label="Stream ID",
+        description="Optional stream identifier",
+        placeholder="streamid"
+    )
+
+    x264_opts: str = Field(
+        default="key-int-max=30 tune=zerolatency speed-preset=slower",
+        label="X264 Options",
+        description="Options for x264enc (eg. \"tune=zerolatency pass=quant quantizer=19\")",
+        placeholder="tune=zerolatency pass=cbr bitrate=8192"
+    )
+
+    h264_profile: Optional[str] = Field(
+        default="main",
+        label="X264 Profile",
+        description="h264 profile to use (high-4:4:4, high-4:2:2, high-10, high, main, baseline, constrained-baseline, high-4:4:4-intra, high-4:2:2-intra, high-10-intra))",
+        placeholder="high"
+    )
+
+    h264_level: Optional[str] = Field(
+        default="3.1",
+        label="X264 Level",
+        description="h264 Level ( eg.: 3.1, 4)",
+        placeholder="3.1"
+    )
+
+    audio_codec: Optional[str] = Field(
+        default="aac",
+        label="Audio Codec",
+        description="audio codec to use (aac, mp2, mp3)",
+        placeholder="aac"
+    )
+    audio_opts: Optional[str] = Field(
+        default="",
+        label="Audio encoder Options",
+        description="options for the audio encoder selected. refer to gstreamer properties.",
+        placeholder="bitrate=192000"
+    )
+    mux_opts: Optional[str] = Field(
+        default="",
+        label="Mpegts Mux Options",
+        description="Mux options for mpegtsmux",
+        placeholder="bitrate=6000000"
+    )
+
+
+from pipelines.outputs.srtserversink import SrtserversinkOutput
+
+@router.put("/srtsink", response_model=SuccessDTO)
+async def create_srtsink_output(request: Request, data: SrtserversinkOutputDTO):
+    handler = request.app.state._state["pipeline_handler"]
+    output = handler.get_pipeline("outputs", data.uid)
+
+    if output:
+        output.data = data
+    else:
+        output = SrtserversinkOutput(data=data)
+        handler.add_pipeline(output)
+
+    await manager.broadcast("CREATE", data)
+
+    return data
---
+++ pipelines/outputs/srtserversink.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from typing import Optional
+from pipeline_handler import HandlerSingleton
+
+from pipelines.outputs.output import Output
+from api.outputs.srtserversink import SrtserversinkOutputDTO
+
+
+class SrtserversinkOutput(Output):
+    data: SrtserversinkOutputDTO
+
+    def build(self):
+
+        pipeline_audio_str = ""
+        handler = HandlerSingleton()
+        input = handler.getpipeline(self.data.src)
+        if self.data.audio_codec == "aac":
+            aenc_str = " voaacenc  ! aacparse ! audio/mpeg, mpegversion=4"
+        if self.data.audio_codec == "mp2":
+            aenc_str = f" avenc_mp2 { self.data.audio_opts } ! audio/mpeg,mpegversion=1,layer=2,channels=2,mode=joint-stereo"
+        if input.has_audio_or_video("audio"):
+                audio_caps = self.get_caps('audio', 'S16LE')
+                pipeline_audio_str = f" {self.get_audio_start()}  audioconvert ! audioresample ! {audio_caps} ! { aenc_str } ! queue ! mux."
+
+        # @TODO add more options to srtserversink
+        self.add_pipeline(self.get_video_start() + f"  videorate ! videoconvert !  videoscale !   { self.get_caps('video', 'I420') }  ! "
+        f" x264enc {self.data.x264_opts}  ! video/x-h264,profile={ self.data.h264_profile } ! queue !  "
+        f" mpegtsmux name=mux {self.data.mux_opts} ! "
+        f" srtserversink name=sink  uri={self.data.uri} "
+        f" { pipeline_audio_str }")
+
+
+    def describe(self):
+        return self.data

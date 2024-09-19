faef86a4219056c723e32c6775bc24b35d28a6af
hatsch <hatsch@servus.at>
2024-09-19 21:15:55 +0200

    update srtserversink.

--- pipelines/outputs/srtserversink.py
+++ pipelines/outputs/srtserversink.py
@@ -20,15 +20,15 @@ class SrtserversinkOutput(Output):
             aenc_str = f" avenc_mp2 { self.data.audio_opts } ! audio/mpeg,mpegversion=1,layer=2,channels=2,mode=joint-stereo"
         if input.has_audio_or_video("audio"):
                 audio_caps = self.get_caps('audio', 'S16LE')
-                pipeline_audio_str = f" {self.get_audio_start()}  audioconvert ! audioresample ! {audio_caps} ! { aenc_str } ! queue ! mux."
+                pipeline_audio_str = f" {self.get_audio_start()}  audioconvert ! audioresample ! {audio_caps} ! { aenc_str } ! queue leaky=downstream ! mux."
+
 
         # @TODO add more options to srtserversink
-        self.add_pipeline(self.get_video_start() + f"  videorate ! videoconvert !  videoscale !   { self.get_caps('video', 'I420') }  ! "
-        f" x264enc {self.data.x264_opts}  ! video/x-h264,profile={ self.data.h264_profile } ! queue !  "
+        self.add_pipeline(self.get_video_start() + f"  videorate ! videoconvert !  videoscale !   { self.get_caps('video', 'I420') }  !"
+        f" x264enc {self.data.x264_opts}  ! video/x-h264,profile={ self.data.h264_profile } ! h264parse ! queue leaky=downstream !  "
         f" mpegtsmux name=mux {self.data.mux_opts} ! "
-        f" srtserversink name=sink  uri={self.data.uri} mode=2 latency={self.data.latency}"
+        f" srtserversink name=sink  uri={self.data.uri} mode=2"
         f" { pipeline_audio_str }")
-
 
     def describe(self):
         return self.data

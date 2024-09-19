0a8d902772f611d5763d76ee347f4d79dafc6e5e
hatsch <hatsch@servus.at>
2024-09-19 21:29:34 +0200

    add queue to output.

--- pipelines/outputs/output.py
+++ pipelines/outputs/output.py
@@ -9,9 +9,9 @@ from pipelines.base import GSTBase
 class Output(GSTBase, ABC):
     data: OutputDTO
     def get_video_start(self) -> str:
-        return f" interpipesrc name=video_{self.data.uid} listen-to=video_{self.data.src} is-live=true format=time allow-renegotiation=true stream-sync=restart-ts leaky-type=downstream ! "
+        return f" interpipesrc name=video_{self.data.uid} listen-to=video_{self.data.src} is-live=true format=time allow-renegotiation=true stream-sync=restart-ts leaky-type=downstream ! queue ! "
     def get_audio_start(self):
-        return f" interpipesrc name=audio_{self.data.uid} listen-to=audio_{self.data.src} is-live=true format=time allow-renegotiation=true stream-sync=restart-ts leaky-type=downstream ! "
+        return f" interpipesrc name=audio_{self.data.uid} listen-to=audio_{self.data.src} is-live=true format=time allow-renegotiation=true stream-sync=restart-ts leaky-type=downstream ! queue ! "
 
     def describe(self):
         return self

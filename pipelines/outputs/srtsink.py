e9d14d0bfe49869e560aba7f2fa1f3376b8f7647
hatsch <hatsch@servus.at>
2024-09-20 10:43:20 +0200

    Revert "add mp2 audio codec and more options for srtsink."
    This reverts commit a6a2917a5d99d0c5c4f191dc9fba1af0835d987c.

--- pipelines/outputs/srtsink.py
+++ pipelines/outputs/srtsink.py
@@ -17,15 +17,12 @@ class SrtsinkOutput(Output):
         input = handler.getpipeline(self.data.src)
         if self.data.audio_codec == "aac":
             aenc_str = " voaacenc  ! aacparse ! audio/mpeg, mpegversion=4"
-        if self.data.audio_codec == "mp2":
-            aenc_str = f" avenc_mp2 { self.data.audio_opts } ! audio/mpeg,mpegversion=1,layer=2,channels=2,mode=joint-stereo"
         if input.has_audio_or_video("audio"):
                 audio_caps = self.get_caps('audio', 'S16LE')
-                pipeline_audio_str = f" {self.get_audio_start()}  audioconvert ! audioresample ! {audio_caps} ! { aenc_str } ! queue ! mux."
+                pipeline_audio_str = f" {self.get_audio_start()}  audioconvert ! audioresample ! {audio_caps} ! { aenc_str } ! mux."
 
         self.add_pipeline(self.get_video_start() + f"  videorate ! videoconvert !  videoscale !   { self.get_caps('video', 'I420') }  ! "
-        f" x264enc {self.data.x264_opts}  ! video/x-h264,profile={ self.data.h264_profile } ! queue !  "
-        f" mpegtsmux name=mux {self.data.mux_opts} ! "
+        f" x264enc {self.data.x264_opts}  ! video/x-h264,profile={ self.data.h264_profile } ! queue !  mpegtsmux name=mux ! "
         f" srtsink name=sink uri={self.data.uri} "
         f" { pipeline_audio_str }")
 

2b091f403eb4e1bb2aaebd19b4d858538802b1bb
hatsch <hatsch@servus.at>
2024-12-11 16:02:06 +0100

    add video caps to testsrc.

--- pipelines/inputs/testsrc.py
+++ pipelines/inputs/testsrc.py
@@ -8,7 +8,7 @@ class TestsrcInput(Input):
     data: TestsrcInputDTO
 
     def build(self):
-        video_pipeline_str = f" videotestsrc do-timestamp=true is_live=true pattern={self.data.pattern}  name=videotestsrc_{self.data.uid}  ! "  + self.get_video_end()
+        video_pipeline_str = f" videotestsrc do-timestamp=true is_live=true pattern={self.data.pattern}  name=videotestsrc_{self.data.uid}  !  {self.get_caps('video') } ! "  + self.get_video_end()
         audio_pipeline_str = f" audiotestsrc do-timestamp=true is-live=true wave={self.data.wave} freq={self.data.freq}  volume={self.data.volume} name=audiotestsrc_{self.data.uid} ! { self.get_caps('audio') } ! " + self.get_audio_end()
         self.add_pipeline(video_pipeline_str + audio_pipeline_str)
 
